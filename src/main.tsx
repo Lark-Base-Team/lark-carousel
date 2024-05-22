155b34b75f520c6bccfb3b73f2e057dcfdcf79ac
Liu Yi <326534215@qq.com>
2024-05-23 00:15:52 +0800

    fix: 引入不存在的包报错

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './i18n.js';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
